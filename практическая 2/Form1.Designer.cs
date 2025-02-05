7fdedc8d428d820739d3ba13e1e6095ad776268f
Arkadiy22-pixel <ananasikzloy@gmail.com>
2025-02-05 15:07:22 +0300

    Add files via upload

---
+++ практическая 2/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace практическая_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBarCapacity = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxОbject = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonСancel = new System.Windows.Forms.Button();
+            this.buttonСamping = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxAddedItems = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вместимость рюкзака:";
+            // 
+            // progressBarCapacity
+            // 
+            this.progressBarCapacity.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.progressBarCapacity.Location = new System.Drawing.Point(331, 62);
+            this.progressBarCapacity.Name = "progressBarCapacity";
+            this.progressBarCapacity.Size = new System.Drawing.Size(432, 39);
+            this.progressBarCapacity.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(417, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Выберите предметы которые хотите взять в поход:";
+            // 
+            // comboBoxОbject
+            // 
+            this.comboBoxОbject.FormattingEnabled = true;
+            this.comboBoxОbject.Location = new System.Drawing.Point(438, 167);
+            this.comboBoxОbject.Name = "comboBoxОbject";
+            this.comboBoxОbject.Size = new System.Drawing.Size(325, 21);
+            this.comboBoxОbject.TabIndex = 4;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.Khaki;
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(438, 224);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(325, 32);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonСancel
+            // 
+            this.buttonСancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonСancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonСancel.Location = new System.Drawing.Point(50, 443);
+            this.buttonСancel.Name = "buttonСancel";
+            this.buttonСancel.Size = new System.Drawing.Size(307, 68);
+            this.buttonСancel.TabIndex = 6;
+            this.buttonСancel.Text = "Отменить поход";
+            this.buttonСancel.UseVisualStyleBackColor = false;
+            // 
+            // buttonСamping
+            // 
+            this.buttonСamping.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonСamping.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonСamping.Location = new System.Drawing.Point(425, 443);
+            this.buttonСamping.Name = "buttonСamping";
+            this.buttonСamping.Size = new System.Drawing.Size(307, 68);
+            this.buttonСamping.TabIndex = 7;
+            this.buttonСamping.Text = "Отправиться в поход";
+            this.buttonСamping.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(438, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(325, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Удалить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxAddedItems
+            // 
+            this.textBoxAddedItems.Location = new System.Drawing.Point(241, 327);
+            this.textBoxAddedItems.Multiline = true;
+            this.textBoxAddedItems.Name = "textBoxAddedItems";
+            this.textBoxAddedItems.Size = new System.Drawing.Size(522, 32);
+            this.textBoxAddedItems.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(14, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Добавленные предметы:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAddedItems);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonСamping);
+            this.Controls.Add(this.buttonСancel);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.comboBoxОbject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBarCapacity);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(816, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBarCapacity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxОbject;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonСancel;
+        private System.Windows.Forms.Button buttonСamping;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxAddedItems;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ практическая 2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace практическая_2
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> ObjectsInBackpack;
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            progressBarCapacity.Minimum = 0;
+            progressBarCapacity.Maximum = 10000;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ObjectsInBackpack = new Dictionary<string, int>()
+            {
+                ["спальный мешок 2 кг"] = 2000,
+                ["салфетки 0.2 кг"] = 200,
+                ["ложка 0.1 кг"] = 150,
+                ["вилка 0.1 кг"] = 150,
+                ["нож 0.33 кг"] = 330,
+                ["антисептик 0.45 кг"] = 450,
+                ["фонарь 0.68 кг"] = 680,
+                ["веревка 0.9 кг"] = 900,
+                ["кружка 0.65 кг"] = 650,
+                ["носки 0.7 кг"] = 700,
+                ["аптечка 1.2 кг"] = 1200,
+                ["палатка 5 кг"] = 5000,
+                ["еда 3 кг"] = 3000,
+                ["дождевик 1.4 кг"] = 1400,
+                ["лопата 2 кг"] = 2000,
+                ["вода 1 кг"] = 1000
+            };
+            foreach (string objects in ObjectsInBackpack.Keys)
+                comboBoxОbject.Items.Add(objects);
+            comboBoxОbject.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            
+            int value = 0;            
+            if (ObjectsInBackpack.TryGetValue(comboBoxОbject.SelectedItem.ToString(), out value))
+            {                
+                progressBarCapacity.Value += value;    
+            }
+            //if (ObjectsInBackpack.TryGetValue(comboBoxОbject.SelectedItem))
+            comboBoxОbject.SelectedIndex = -1;
+            //if (progressBarCapacity.Value > 12)
+            //    MessageBox.Show("Рюкзак заполнен!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int value = 0;
+            if (ObjectsInBackpack.TryGetValue(comboBoxОbject.SelectedItem.ToString(), out value))
+            { progressBarCapacity.Value -= value; }
+        }
+
+    }
+}
